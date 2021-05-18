5833db5b50bdcfccaa8c6e89cef1edf6454a0f6f
Senghor Saldès <senghor.pro@gmail.com>
2021-05-18 10:28:32 +0200

    Commit1ofd

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll(["./", "./CSS/style.css", "./images/logo.png"]);
+        })
+    );
+});
+
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+
+        })
+    );
+     
+});
